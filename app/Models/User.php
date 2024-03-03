64a770215657ae383183fe9e2d8f3247668006af
Joseph Jett Abela <jetpackzabela12@gmail.com>
2024-03-03 16:50:34 +0800

    Backend Login Adding Auth Implementing Role And Permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,26 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'username',
         'password',
+        'roles_id'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
--- resources/views/Admin/Pages/Login/login.blade.php
+++ resources/views/Admin/Pages/Login/login.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>Animated Login Form</title>
 	<link rel="stylesheet" type="text/css" href="{{asset ('css/style.css') }}">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	@include ('admin_components.head_script')
 </head>
 <body>
 	<img class="wave" src="{{asset ('img/wave.png')}}">
@@ -14,7 +17,7 @@
 			<img src="{{asset ('img/bg.svg')}}">
 		</div>
 		<div class="login-content">
-			<form action="#">
+			<form action="{{ route('user_login') }}" class="formPost">
 				<img src="{{asset ('img/avatar.svg')}}">
 				<h2 class="title">Welcome</h2>
            		<div class="input-div one">
@@ -23,7 +26,7 @@
            		   </div>
            		   <div class="div">
            		   		<h5>Username</h5>
-           		   		<input type="text" class="input">
+           		   		<input type="text" class="input" name="username" required>
            		   </div>
            		</div>
            		<div class="input-div pass">
@@ -32,7 +35,7 @@
            		   </div>
            		   <div class="div">
            		    	<h5>Password</h5>
-           		    	<input type="password" class="input">
+           		    	<input type="password" class="input" name="password" required>
             	   </div>
             	</div>
             	<input type="submit" class="btn" value="Login">
@@ -41,4 +44,50 @@
     </div>
     <script type="text/javascript" src="{{asset ('js/main.js')}}"></script>
 </body>
+<script>
+	$('.formPost').on('submit',function(e) {
+      e.preventDefault();
+
+      const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 1500
+      });
+      
+      $.ajax({
+          type     : "POST",
+          cache    : false,
+          url      : $(this).attr('action'),
+          data     : $(this).serialize(),
+          headers: {
+              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          },
+          success  : function(data) {
+
+              switch(data['response']) {
+                  case 1:
+                          Toast.fire({
+                              icon: 'success',
+                              title: '<p class="text-center pt-2 text-bold text-black">' +data['message']+ '</p>'
+                          });
+
+                          setTimeout(function() {
+                              window.location.href = data['path'];
+                          },1500);
+
+                      break;
+                  default:
+                          Toast.fire({
+                              icon: 'error',
+                              title: '<p class="text-center pt-2">' +data['message']+ '</p>'
+                          });
+                      break;
+              }
+
+          }
+      });
+
+  });
+</script>
 </html>
