8030a3df05240669f123ac7faf31904c79c5f623
Joseph Jett Abela <jetpackzabela12@gmail.com>
2024-03-04 22:29:53 +0800

    Removing default value in password

--- resources/views/Admin/Pages/Login/login.blade.php
+++ resources/views/Admin/Pages/Login/login.blade.php
@@ -35,7 +35,7 @@
            		   </div>
            		   <div class="div">
            		    	<h5>Password</h5>
-           		    	<input type="password" class="input" name="password" value="03blS3gj" required>
+           		    	<input type="password" class="input" name="password" required>
             	   </div>
             	</div>
             	<input type="submit" class="btn" value="Login">
